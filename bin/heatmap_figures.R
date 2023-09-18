8a24ececcac0807bf6c74336b4877587181f7a1d
hannahgoldswain <80838990+hannahgoldswain@users.noreply.github.com>
2023-09-18 12:27:37 +0100

    Updating comments for heatmap figures

--- bin/heatmap_figures.R
+++ bin/heatmap_figures.R
@@ -1,4 +1,5 @@
-# heatmap to compare syn and non syn mutations across proteins
+# heatmaps to compare synonymous and non-synonymous mutations across proteins and transitions and transversion mutations
+# read in concatenated parse files outputted from Syn_NonSyn_parse_aa_V3.pl 
 
 library(ggplot2)
 library(ggrepel)
@@ -12,16 +13,15 @@ library("pheatmap")
 ### SYN NONSYN HEATMAP ###
 # use cat parse.txt file 
 
-parse_all_nim <- read.delim("/home/hannahg/projects/dstl_project/data/nimagen/DiversiTools/nimnan25_parse.txt", sep="\t")
-parse15N_nim <- read.delim("/home/hannahg/projects/dstl_project/data/nimagen/DiversiTools/nim15%_parse.txt", sep="\t")
+parse15N_nim <- read.delim("/home/hannahg/projects/dstl_project/data/nimagen/DiversiTools/nim15%_parse.txt", sep="\t") # read in concatenated parse file
 
-parse15N_nim$CntNonSyn<-as.numeric(parse15N_nim$CntNonSyn)
-parse15N_nim$CntSyn<-as.numeric(parse15N_nim$CntSyn)
-parse15N_nim$AAcoverage<-as.numeric(parse15N_nim$AAcoverage)
-parse15N_nim <- parse15N_nim %>% filter(AAcoverage > 20)
-parse15N_nim$prop_syn <- parse15N_nim$CntSyn / parse15N_nim$AAcoverage
-parse15N_nim$prop_nonsyn <- parse15N_nim$CntNonSyn / parse15N_nim$AAcoverage
-parse15N_nim$Protein[parse15N_nim$Protein == "nsp12_1"] <- "NSP12"
+parse15N_nim$CntNonSyn<-as.numeric(parse15N_nim$CntNonSyn) # change variable to numeric for calculations
+parse15N_nim$CntSyn<-as.numeric(parse15N_nim$CntSyn) # change variable to numeric for calculations
+parse15N_nim$AAcoverage<-as.numeric(parse15N_nim$AAcoverage) # change variable to numeric for calculations
+parse15N_nim <- parse15N_nim %>% filter(AAcoverage > 20) # filter at a coverage of 20
+parse15N_nim$prop_syn <- parse15N_nim$CntSyn / parse15N_nim$AAcoverage # work out proportion of synonymous mutations using counts and coverage 
+parse15N_nim$prop_nonsyn <- parse15N_nim$CntNonSyn / parse15N_nim$AAcoverage # work out proportion of non-synonymous mutations using counts and coverage 
+parse15N_nim$Protein[parse15N_nim$Protein == "nsp12_1"] <- "NSP12" # rename protein names from the coding region text file for plots 
 parse15N_nim$Protein[parse15N_nim$Protein == "nsp12_2"] <- "NSP12"
 parse15N_nim$Protein[parse15N_nim$Protein == "nsp1"] <- "NSP1"
 parse15N_nim$Protein[parse15N_nim$Protein == "nsp2"] <- "NSP2"
@@ -38,11 +38,12 @@ parse15N_nim$Protein[parse15N_nim$Protein == "nsp14"] <- "NSP14"
 parse15N_nim$Protein[parse15N_nim$Protein == "nsp15"] <- "NSP15"
 parse15N_nim$Protein[parse15N_nim$Protein == "nsp16"] <- "NSP16"
 
-# aim to calculate mean of each per protein 
+# calculate mean of each type of mutation per protein 
 
 Mean_syn <- as.data.frame(parse15N_nim %>% group_by(Protein, Sample) %>% summarize(Mean_syn=mean(prop_syn, na.rm=T))) 
 Mean_nonsyn <- as.data.frame(parse15N_nim %>% group_by(Protein, Sample) %>% summarize(Mean_nonsyn=mean(prop_nonsyn, na.rm=T))) 
 
+# change names from barcodes generated in sequencing to participant numbers and samples
 Mean_nonsyn$Sample[Mean_nonsyn$Sample == "3636_TAGTCGTCAA-ATGGCTCGGT_L002.final"] <- "01 S1"
 Mean_nonsyn$Sample[Mean_nonsyn$Sample == "3637_TGGACTAATT-CAGCGGATAA_L002.final"] <- "01 S2"
 Mean_nonsyn$Sample[Mean_nonsyn$Sample == "3639_ATCTTATGAT-GCCTGGCAGT_L002.final"] <- "02 S1"
@@ -73,6 +74,7 @@ Mean_nonsyn$Sample[Mean_nonsyn$Sample == "3679_TAGTTATGCG-CTGGCTTAGT_L002.final"
 Mean_nonsyn$Sample[Mean_nonsyn$Sample == "3682_GTCCGTAAGG-CGACCTATAC_L002.final"] <- "16 S1"
 Mean_nonsyn$Sample[Mean_nonsyn$Sample == "3683_CCGGTCGACG-GCTGCATAGC_L002.final"] <- "16 S2"
 
+# change names from barcodes generated in sequencing to participant numbers and samples
 Mean_syn$Sample[Mean_syn$Sample == "3636_TAGTCGTCAA-ATGGCTCGGT_L002.final"] <- "01 S1"
 Mean_syn$Sample[Mean_syn$Sample == "3637_TGGACTAATT-CAGCGGATAA_L002.final"] <- "01 S2"
 Mean_syn$Sample[Mean_syn$Sample == "3639_ATCTTATGAT-GCCTGGCAGT_L002.final"] <- "02 S1"
@@ -103,6 +105,7 @@ Mean_syn$Sample[Mean_syn$Sample == "3679_TAGTTATGCG-CTGGCTTAGT_L002.final"] <- "
 Mean_syn$Sample[Mean_syn$Sample == "3682_GTCCGTAAGG-CGACCTATAC_L002.final"] <- "16 S1"
 Mean_syn$Sample[Mean_syn$Sample == "3683_CCGGTCGACG-GCTGCATAGC_L002.final"] <- "16 S2"
 
+# change variable to factor to enable ordering of proteins in heatmap
 Mean_syn$Protein <- as.factor(Mean_syn$Protein)
 Mean_syn$Protein <- factor(Mean_syn$Protein, levels= c("5' UTR", "NSP1", "NSP2", "NSP3", "NSP4", "NSP5", "NSP6", "NSP7", "NSP8", "NSP9", "NSP10", "NSP12", "NSP13", "NSP14", "NSP15", "NSP16", "S", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF10", "3' UTR"))
 Mean_nonsyn$Protein <- as.factor(Mean_nonsyn$Protein)
@@ -110,44 +113,40 @@ Mean_nonsyn$Protein <- factor(Mean_nonsyn$Protein, levels= c("5' UTR", "NSP1", "
 
 
 ## dN/dS ratio using tstv ratio script dNonsyn / dSyn
-# use parse15N_nim
+# use parse15N_nim formatted above 
 
 metadata_dstl <- read.csv("/home/hannahg/projects/dstl_project/data/nimagen/nimagen_metadata_bam.csv") # metadata sheet with sample info read in
-parse15N_nim <- merge(metadata_dstl, parse15N_nim, by = "Sample")
-parse15N_nim$Protein <- factor(parse15N_nim$Protein, levels = c("5'UTR", "NSP1", "NSP2", "NSP3", "NSP4", "NSP5", "NSP6", "NSP7", "NSP8", "NSP9", "NSP10", "NSP12", "NSP13", "NSP14", "NSP15", "NSP16", "S", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF10", "3'UTR"))
+parse15N_nim <- merge(metadata_dstl, parse15N_nim, by = "Sample") # merge metadata to formatted dataframe above
+parse15N_nim$Protein <- factor(parse15N_nim$Protein, levels = c("5'UTR", "NSP1", "NSP2", "NSP3", "NSP4", "NSP5", "NSP6", "NSP7", "NSP8", "NSP9", "NSP10", "NSP12", "NSP13", "NSP14", "NSP15", "NSP16", "S", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF10", "3'UTR")) # set order of proteins for heatmap
 
 protlengths <- parse15N_nim %>% group_by(Sample, Protein) %>% count(AAPosition) %>% tally
 protlengths<- rename(protlengths, ProtLength=n)
 parse15N_nim<- merge(parse15N_nim, protlengths, by=c("Sample","Protein"))
 
-parse15N_nim$CntNonSyn<-as.numeric(parse15N_nim$CntNonSyn)
+parse15N_nim$CntNonSyn<-as.numeric(parse15N_nim$CntNonSyn) # change variables to numeric to allow calculations
 parse15N_nim$CntSyn<-as.numeric(parse15N_nim$CntSyn)
 parse15N_nim$AAcoverage<-as.numeric(parse15N_nim$AAcoverage)
-parse15N_nim <- parse15N_nim %>% filter(AAcoverage > 20)
-parse15N_nim <- parse15N_nim %>% filter(Protein != "NA")
-parse15N_nim$prop_nonsyn <- parse15N_nim$CntNonSyn / parse15N_nim$AAcoverage
+parse15N_nim <- parse15N_nim %>% filter(AAcoverage > 20) # filter coverage at 20
+parse15N_nim <- parse15N_nim %>% filter(Protein != "NA") # remove NAs
+parse15N_nim$prop_nonsyn <- parse15N_nim$CntNonSyn / parse15N_nim$AAcoverage # calculate proportions
 parse15N_nim$prop_syn <- parse15N_nim$CntSyn / parse15N_nim$AAcoverage
 
-## Iahs mean tstv per protein 
+## Mean tstv per protein based on IDB script, works out means relative to protein length
 mean_nonsyn_perprot <- parse15N_nim %>% filter(AAcoverage >20) %>% group_by(Protein, Sample, ProtLength) %>% 
   summarize(Meannonsyn=mean(prop_nonsyn, na.rm=T ), stdev=sd(prop_nonsyn, na.rm=T), across())
 mean_nonsyn_perprot$Meannonsyn_pnorm <- mean_nonsyn_perprot$Meannonsyn / mean_nonsyn_perprot$ProtLength
-#mean_ts_perprot$Meants_pnorm_scale <- scale(mean_ts_perprot$Meants_pnorm)
-#mean_ts_perprot$MeantsNorm <- scale(mean_ts_perprot$Meants)
 
 mean_syn_perprot <- parse15N_nim %>% filter(AAcoverage >20) %>% group_by(Protein, Sample, ProtLength) %>% 
   summarize(Meansyn=mean(prop_syn, na.rm=T ), stdev=sd(prop_syn, na.rm=T), across())
 mean_syn_perprot$Meansyn_pnorm <- mean_syn_perprot$Meansyn / mean_syn_perprot$ProtLength
-#mean_tv_perprot$Meantv_pnorm_scale <- scale(mean_tv_perprot$Meantv_pnorm)
-#mean_tv_perprot$MeantvNorm <- scale(mean_tv_perprot$Meantv)
 
-meansns_perprot <- merge(mean_nonsyn_perprot, mean_syn_perprot, by = c("Sample", "Protein", "ProtLength", "AAPosition", "Timepoint", "Participant"))
+meansns_perprot <- merge(mean_nonsyn_perprot, mean_syn_perprot, by = c("Sample", "Protein", "ProtLength", "AAPosition", "Timepoint", "Participant")) # merge columns necessary
 
-meansns_perprot$snsratio_norm <- meansns_perprot$MeannonsynNorm / meansns_perprot$MeansynNorm
+meansns_perprot$snsratio_norm <- meansns_perprot$MeannonsynNorm / meansns_perprot$MeansynNorm # calculate ratio
 meansns_perprot$snsratio_norm <- meansns_perprot$Meannonsyn_pnorm / meansns_perprot$Meansyn_pnorm # for unscaled
 names(meansns_perprot)[names(meansns_perprot) == "snsratio_norm"] <- "Ratio"
 
-## use this one ?? ##
+# generate heatmap and facet with timepoint
 dnds<- ggplot(meansns_perprot, aes(Protein, Participant, fill= Ratio)) + 
   geom_tile(colour="black") + scale_fill_gradient(low= "white", high= "blue") + ylab("Participant") + xlab("Protein") +
   ggtitle("dN/dS Ratio") + theme_bw() + theme(axis.text.x = element_text(angle=90, vjust= 0.7)) + facet_wrap(~Timepoint) 
@@ -156,11 +155,14 @@ ggsave(filename= "dndsratio_nim_hm_20X.tiff", plot= dnds, device='tiff', dpi=300
 
 
 
-### INDELS HEATMAP ###
+### HEATMAP to look at transitions and transversions ###
+# use entropy.txt file output from DiversiTools
 # getting position on entropy file for nsps by reading in individually and then rbinding them all at the end
+# can also use to investigate indels
 
-nsps <- read.csv("/home/hannahg/projects/dstl_project/data/below_15%_N/protein_position.csv")
+nsps <- read.csv("/home/hannahg/projects/dstl_project/data/below_15%_N/protein_position.csv") # read in protein position file
 
+# bind protein column to entropy files
 ent_36 <- read.delim("/home/hannahg/projects/dstl_project/data/nimagen/DiversiTools/3636_TAGTCGTCAA-ATGGCTCGGT_L002.final_entropy.txt", sep="\t")
 ent_36_prot <- cbind(ent_36, nsps["Protein"])
 ent_37 <- read.delim("/home/hannahg/projects/dstl_project/data/nimagen/DiversiTools/3637_TGGACTAATT-CAGCGGATAA_L002.final_entropy.txt", sep="\t")
@@ -220,9 +222,10 @@ ent_82_prot <- cbind(ent_82, nsps["Protein"])
 ent_83 <- read.delim("/home/hannahg/projects/dstl_project/data/nimagen/DiversiTools/3683_CCGGTCGACG-GCTGCATAGC_L002.final_entropy.txt", sep="\t")
 ent_83_prot <- cbind(ent_83, nsps["Protein"])
 
-all_ent <- rbind(ent_36_prot, ent_37_prot, ent_39_prot, ent_40_prot, ent_43_prot, ent_44_prot, ent_47_prot, ent_49_prot, ent_50_prot, ent_51_prot, ent_52_prot, ent_53_prot, ent_54_prot, ent_59_prot, ent_60_prot, ent_61_prot, ent_62_prot, ent_64_prot, ent_65_prot, ent_66_prot, ent_67_prot, ent_68_prot, ent_70_prot, ent_74_prot, ent_78_prot, ent_79_prot, ent_80_prot, ent_82_prot, ent_83_prot)
+# rbind all dataframes
 nim15N_ent <- rbind(ent_36_prot, ent_37_prot, ent_39_prot, ent_40_prot, ent_43_prot, ent_44_prot, ent_47_prot, ent_49_prot, ent_50_prot, ent_51_prot, ent_52_prot, ent_53_prot, ent_54_prot, ent_59_prot, ent_60_prot, ent_61_prot, ent_62_prot, ent_65_prot, ent_67_prot, ent_68_prot, ent_70_prot, ent_74_prot, ent_79_prot, ent_82_prot, ent_83_prot)
 
+# rename protein columns for figure
 nim15N_ent$Protein[nim15N_ent$Protein == "nsp12"] <- "NSP12"
 nim15N_ent$Protein[nim15N_ent$Protein == "nsp1"] <- "NSP1"
 nim15N_ent$Protein[nim15N_ent$Protein == "nsp2"] <- "NSP2"
@@ -240,51 +243,43 @@ nim15N_ent$Protein[nim15N_ent$Protein == "nsp15"] <- "NSP15"
 nim15N_ent$Protein[nim15N_ent$Protein == "nsp16"] <- "NSP16"
 
 
-### TSTV ### 
-# use all_ent or nim15N_ent from above 
-
-### for nim15N_ent creating mean per protein of tstv 25/10/22 ###
+### Create mean per protein of tstv similar to synonymous/non-synonymous calculations ###
 
 metadata_dstl <- read.csv("/home/hannahg/projects/dstl_project/data/nimagen/nimagen_metadata.csv") # metadata sheet with sample info read in
-nim15N_ent <- merge(metadata_dstl, nim15N_ent, by = "Sample")
-nim15N_ent$Protein <- factor(nim15N_ent$Protein, levels = c("5'UTR", "NSP1", "NSP2", "NSP3", "NSP4", "NSP5", "NSP6", "NSP7", "NSP8", "NSP9", "NSP10", "NSP12", "NSP13", "NSP14", "NSP15", "NSP16", "S", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF10", "3'UTR"))
+nim15N_ent <- merge(metadata_dstl, nim15N_ent, by = "Sample") # merge metadata and dataframe generated above
+nim15N_ent$Protein <- factor(nim15N_ent$Protein, levels = c("5'UTR", "NSP1", "NSP2", "NSP3", "NSP4", "NSP5", "NSP6", "NSP7", "NSP8", "NSP9", "NSP10", "NSP12", "NSP13", "NSP14", "NSP15", "NSP16", "S", "ORF3a", "E", "M", "ORF6", "ORF7a", "ORF7b", "ORF8", "N", "ORF10", "3'UTR")) # order proteins for figure
 
 protlengths <- nim15N_ent %>% group_by(Sample, Protein) %>% count(Position) %>% tally
 protlengths<- rename(protlengths, ProtLength=n)
 nim15N_ent<- merge(nim15N_ent, protlengths, by=c("Sample","Protein"))
 
-nim15N_ent$CntTs<-as.numeric(nim15N_ent$CntTs)
+nim15N_ent$CntTs<-as.numeric(nim15N_ent$CntTs) # make variable numeric for calculations
 nim15N_ent$CntTv<-as.numeric(nim15N_ent$CntTv)
 nim15N_ent$Coverage<-as.numeric(nim15N_ent$Coverage)
-nim15N_ent <- nim15N_ent %>% filter(Coverage > 20)
-nim15N_ent <- nim15N_ent %>% filter(Protein != "NA")
-nim15N_ent$prop_ts <- nim15N_ent$CntTs / nim15N_ent$Coverage
+nim15N_ent <- nim15N_ent %>% filter(Coverage > 20) # filter coverage at 20
+nim15N_ent <- nim15N_ent %>% filter(Protein != "NA") # remove NAs
+nim15N_ent$prop_ts <- nim15N_ent$CntTs / nim15N_ent$Coverage # calculate proportions
 nim15N_ent$prop_tv <- nim15N_ent$CntTv / nim15N_ent$Coverage
 
-## Iahs mean tstv per protein 
+## Mean tstv per protein as above adapted from IDB script
 mean_ts_perprot <- nim15N_ent %>% filter(Coverage >20) %>% group_by(Protein, Sample, ProtLength) %>% 
   summarize(Meants=mean(prop_ts, na.rm=T ), stdev=sd(prop_ts, na.rm=T), across())
 mean_ts_perprot$Meants_pnorm <- mean_ts_perprot$Meants / mean_ts_perprot$ProtLength
-#mean_ts_perprot$Meants_pnorm_scale <- scale(mean_ts_perprot$Meants_pnorm)
-#mean_ts_perprot$MeantsNorm <- scale(mean_ts_perprot$Meants)
 
 mean_tv_perprot <- nim15N_ent %>% filter(Coverage >20) %>% group_by(Protein, Sample, ProtLength) %>% 
   summarize(Meantv=mean(prop_tv, na.rm=T ), stdev=sd(prop_tv, na.rm=T), across())
 mean_tv_perprot$Meantv_pnorm <- mean_tv_perprot$Meantv / mean_tv_perprot$ProtLength
-#mean_tv_perprot$Meantv_pnorm_scale <- scale(mean_tv_perprot$Meantv_pnorm)
-#mean_tv_perprot$MeantvNorm <- scale(mean_tv_perprot$Meantv)
 
-meantstv_perprot <- merge(mean_ts_perprot, mean_tv_perprot, by = c("Sample", "Protein", "ProtLength", "Position", "Timepoint", "Patient", "Ct_value", "DPI", "Participant"))
+meantstv_perprot <- merge(mean_ts_perprot, mean_tv_perprot, by = c("Sample", "Protein", "ProtLength", "Position", "Timepoint", "Patient", "Ct_value", "DPI", "Participant")) # merge necessary columns
 
-meantstv_perprot$tstvratio_norm <- meantstv_perprot$MeantsNorm / meantstv_perprot$MeantvNorm
+meantstv_perprot$tstvratio_norm <- meantstv_perprot$MeantsNorm / meantstv_perprot$MeantvNorm # calculate ratios
 meantstv_perprot$tstvratio_norm <- meantstv_perprot$Meants_pnorm / meantstv_perprot$Meantv_pnorm # for unscaled
 names(meantstv_perprot)[names(meantstv_perprot) == "tstvratio_norm"] <- "Ratio"
 
-                                      ## use this one ?? ##
+## plot heatmap
 tstvvv <- ggplot(meantstv_perprot, aes(Protein, Participant, fill= Ratio)) + 
   geom_tile(colour="black") + scale_fill_gradient(low= "white", high= "blue") + ylab("Participant") + xlab("Protein") +
   ggtitle("Ts/Tv Ratio") + theme_bw() + theme(axis.text.x = element_text(angle=90, vjust= 0.7)) + facet_wrap(~Timepoint) 
-
 
 ggsave(filename= "tstvratio_nim_hm_20X.tiff", plot= tstvvv, device='tiff', dpi=300, width=11.5, height=7)
 
